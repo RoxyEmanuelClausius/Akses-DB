fb7ee03550762aa1c82a78444b3e644fc1c7b169
Bimantara26 <bimantara1997@yahoo.com>
2019-12-07 00:20:50 +0700

    first commit

---
+++ app/src/main/java/com/example/koneksidatabase/Fakultas.kt
@@ -0,0 +1,6 @@
+package com.example.koneksidatabase
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+data class Fakultas(val id_fakultas: String, val kode_fakultas:String?, val nama_fakultas:String?) {
+}
